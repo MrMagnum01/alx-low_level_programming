70e6e1adda729a79c3bd6f3182c461017246dcac
Mahmoud Filali <89471146+MrMagnum01@users.noreply.github.com>
2023-06-16 12:56:03 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,16 +9,16 @@
  */
 int main(void)
 {
-0-positive_or_negative.c int n;
+	int n;
 
-0-positive_or_negative.c srand(time(0));
-0-positive_or_negative.c n = rand() - RAND_MAX / 2;
-0-positive_or_negative.c printf("Last digit of %d is %d ", n, n % 10);
-0-positive_or_negative.c if (n % 10 > 5)
-0-positive_or_negative.c 0-positive_or_negative.c printf("and is greater than 5\n");
-0-positive_or_negative.c else if (n % 10 == 0)
-0-positive_or_negative.c 0-positive_or_negative.c printf("and is 0\n");
-0-positive_or_negative.c else if (n % 10 < 6 && n % 10 != 0)
-0-positive_or_negative.c 0-positive_or_negative.c printf("and is less than 6 and not 0\n");
-0-positive_or_negative.c return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
 }
