1c0967da50523775871324430474586d30820ae7
Mahmoud Filali <89471146+MrMagnum01@users.noreply.github.com>
2023-06-20 22:54:01 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,11 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * main - causes an infinite loop
+ * Return: 0
  */
+
 int main(void)
 {
-    return (0);
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
