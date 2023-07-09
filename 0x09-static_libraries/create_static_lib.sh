f7c62a3a54dadc96df358547a108b177b83f3c23
Your Name <you@example.com>
2023-07-08 17:54:19 -0700

    task

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+
+# Compile each .c file into object files
+for file in *.c; do
+    gcc -c "$file"
+done
+
+# Create the static library liball.a from the object files
+ar rcs liball.a *.o
+
+# Clean up - remove the object files
+rm *.o
+
